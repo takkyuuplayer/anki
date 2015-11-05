152501202aa3229ab76706b44706a56cf4e45d4b
takkyuuplayer <takkyuuplayer@gmail.com>
2015-11-06 02:22:32 +0900

    Remove kana

--- bin/create-anki-tsv-by-alc.pl
+++ bin/create-anki-tsv-by-alc.pl
@@ -9,16 +9,21 @@ while (<>) {
     my $res = $alc->do_request($_);
     chomp;
 
+    unless ($res) {
+        say join("\t", $_, 'N/A: Please define manually');
+        next;
+    }
     my $definition = $alc->parse_definition($res);
 
     if ($definition) {
         my $content = $definition->content;
         $content =~ s/\r|\n//g;
+        $content =~ s|<span class="kana">.+?</span>||g;
         say join("\t", $_, $content);
     }
     else {
         say join("\t", $_, 'N/A: Please define manually');
     }
 
-    sleep 10;
+    sleep 30;
 }
